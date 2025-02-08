61fff91c1468c40ce419c39cb4331db8d58e2247
・ω・（竹子） <you948712@gmail.com>
2025-02-08 15:08:49 +0800

    add: update

--- web/radar.js
+++ web/radar.js
@@ -67,8 +67,62 @@ const map = new maplibregl.Map({
     zoom: 6.6
 });
 
+const resetButton = document.createElement('button');
+resetButton.id = 'reset-position';
+resetButton.innerHTML = `
+    <svg
+        width="20"
+        height="20"
+        viewBox="0 0 100 100"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        aria-hidden="true"
+        role="img"
+        class="iconify iconify--gis"
+        preserveAspectRatio="xMidYMid meet"
+    >
+        <path
+            d="M43 0v13.166C27.944 16.03 16.03 27.944 13.166 43H0v14h13.166C16.03 72.056 27.944 83.97 43 86.834V100h14V86.834C72.056 83.97 83.97 72.056 86.834 57H100V43H86.834C83.97 27.944 72.056 16.03 57 13.166V0H43zm7 22.5A27.425 27.425 0 0 1 77.5 50A27.425 27.425 0 0 1 50 77.5A27.425 27.425 0 0 1 22.5 50A27.425 27.425 0 0 1 50 22.5z"
+            fill="currentColor"
+        ></path>
+        <circle r="15" cy="50" cx="50" fill="currentColor"></circle>
+    </svg>
+`;
+document.body.appendChild(resetButton);
+
+resetButton.addEventListener('click', () => {
+    map.jumpTo({
+        center: [120.2, 23.6],
+        zoom: 6.6
+    });
+});
+
+const updateButton = document.createElement('button');
+updateButton.id = 'update-position';
+updateButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M480-120q-75 0-140.5-28.5t-114-77q-48.5-48.5-77-114T120-480q0-75 28.5-140.5t77-114q48.5-48.5 114-77T480-840q82 0 155.5 35T760-706v-94h80v240H600v-80h110q-41-56-101-88t-129-32q-117 0-198.5 81.5T200-480q0 117 81.5 198.5T480-200q105 0 183.5-68T756-440h82q-15 137-117.5 228.5T480-120Zm112-192L440-464v-216h80v184l128 128-56 56Z"/></svg>`;
+document.body.appendChild(updateButton);
+
+updateButton.addEventListener('click', async () => {
+    const response = await fetch('https://api.exptech.dev/api/v1/tiles/radar/list');
+    const timeList = await response.json();
+    const latestTime = timeList[timeList.length - 1];
+
+    const timeDisplay = document.getElementById('time-display');
+    const date = new Date(parseInt(latestTime));
+    timeDisplay.textContent = date.getFullYear() + '-' + 
+        String(date.getMonth() + 1).padStart(2, '0') + '-' +
+        String(date.getDate()).padStart(2, '0') + ' ' +
+        String(date.getHours()).padStart(2, '0') + ':' +
+        String(date.getMinutes()).padStart(2, '0');
+    
+    map.getSource('radarTiles').setTiles([
+        `https://api-1.exptech.dev/api/v1/tiles/radar/${latestTime}/{z}/{x}/{y}.png`
+    ]);
+});
+
 let opacity = 1;
 let isOn = true;
+
 
 function updateOpacity() {
     map.setPaintProperty('tsunami', 'line-opacity', opacity);
